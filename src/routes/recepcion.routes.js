27851e3c28b6cff17edd7d1cc6d6f3e97ed3bf34
NannoFloresCif <hernan.fcc@gmail.com>
2025-09-26 07:03:27 -0300

    actualizacion back recepciones

--- src/routes/recepcion.routes.js
+++ src/routes/recepcion.routes.js
@@ -5,6 +5,6 @@ const { verificarToken } = require('../middleware/auth.middleware.js');
 const { autorizar } = require('../middleware/autorizacion.middleware.js');
 
 // Roles: 3 (Jefe Sucursal), 4 (Administrativo)
-router.post('/recepciones', [verificarToken, autorizar([3, 4])], recepcionController.registrarRecepcion);
+router.post('/recepciones', [verificarToken, autorizar([4])], recepcionController.registrarRecepcion);
 
 module.exports = router;
